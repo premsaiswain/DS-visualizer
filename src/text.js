fa540e857712f4cf1ca39a45bb0b50e3ea6df5da
prem sai swain <premsaiswain@gmail.com>
2021-08-02 08:30:57 +0530

    Linear search demo 2

---
+++ src/text.js
@@ -0,0 +1,29 @@
+ {/* <motion.div className="box"
+             drag
+            animate={{
+                x:work?1000:0,
+                opacity:1,
+                rotate:work?360:0,
+                
+            }}
+            initial={{
+                opacity:0.2,
+                backgroundColor:"aqua"
+            }}
+            transition={{
+                type:'spring',
+                stiffness:200,
+                damping:60,
+                backgroundColor:"red"
+            }}
+            whileHover={{
+                backgroundColor:"red",
+                scale: 1.1
+            }}
+            whileTap={{
+                scale:0.8
+            }}
+            onClick={()=> setWork(work?false:true)}
+            >
+           <h1>Stack</h1>
+            </motion.div> */}
